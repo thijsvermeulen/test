bb378c1f02af9f58d49e43f225487fb26c3c17a6
Thijs Vermeulen <thijsver@xs4all.nl>
2018-02-18 23:38:26 +0100

    minor structure change

--- public/index.php
+++ public/index.php
@@ -3,7 +3,5 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-require '../vendor/autoload.php';
 require "../src/app.php";
-
 run();
--- src/app.php
+++ src/app.php
@@ -1,5 +1,6 @@
 <?php
 
+require '../vendor/autoload.php';
 require 'routes.php';
 
 function run() {
