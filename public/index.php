4e543bfcb3cec52a037a0505714989a24b3df2f8
Thijs Vermeulen <thijsver@xs4all.nl>
2018-02-18 22:53:13 +0100

    added config

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+
+require '../vendor/autoload.php';
 require "../src/app.php";
+
+run();
--- src/app.php
+++ src/app.php
@@ -1,16 +1,31 @@
 <?php
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
+function run() {
 
-require '../vendor/autoload.php';
 
-$app = new \Slim\App;
 
-$app->get('/hello/{name}', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
+    $eloquent = new Illuminate\Database\Capsule\Manager;
+    $eloquent->addConnection([
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'database',
+        'username'  => 'root',
+        'password'  => 'a3d5g7',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix'    => '',
+    ]);
+    $eloquent->setAsGlobal();
+    $eloquent->bootEloquent();
 
-$app->run();
+    $slim = new \Slim\App;
+
+    $slim->get('/hello/{name}', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, array $args) {
+        $name = $args['name'];
+        $response->getBody()->write("Hello, $name");
+        return $response;
+    });
+
+    return $slim;
+}
+
