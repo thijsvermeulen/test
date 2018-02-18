08e0de857522177bcb2ff6db6a6f43df991a9541
Thijs Vermeulen <thijsver@xs4all.nl>
2018-02-18 23:43:28 +0100

    simplified

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 error_reporting(-1);
-ini_set('display_errors', 'On');
+ini_set('display_errors', '1');
 
 require "../src/app.php";
-run();
--- src/app.php
+++ src/app.php
@@ -1,30 +1,46 @@
 <?php
 
 require '../vendor/autoload.php';
-require 'routes.php';
 
-function run() {
+/* ==== CONF ==== */
 
-    $conf = array_merge( parse_ini_file('../conf-default.ini'), parse_ini_file('../conf-local.ini'));
+$conf = array_merge( parse_ini_file('../conf-default.ini'), parse_ini_file('../conf-local.ini'));
 
-    $eloquent = new Illuminate\Database\Capsule\Manager;
-    $eloquent->addConnection([
-        'driver'    => 'mysql',
-        'host'      => $conf['db_host'],
-        'database'  => $conf['db_name'],
-        'username'  => $conf['db_user'],
-        'password'  => $conf['db_pass'],
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
-    ]);
-    $eloquent->setAsGlobal();
-    $eloquent->bootEloquent();
 
-    $slim = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
+/* ==== ELOQUENT ==== */
 
-    register_routes($slim);
+$eloquent = new Illuminate\Database\Capsule\Manager;
+$eloquent->addConnection([
+    'driver'    => 'mysql',
+    'host'      => $conf['db_host'],
+    'database'  => $conf['db_name'],
+    'username'  => $conf['db_user'],
+    'password'  => $conf['db_pass'],
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+$eloquent->setAsGlobal();
+$eloquent->bootEloquent();
 
-    $slim->run();
-}
 
+/* ==== SLIM ==== */
+
+$slim = new \Slim\App(['settings' => ['displayErrorDetails' => true]]);
+
+$slim->get('/hello/{name}', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, array $args) {
+    $name = $args['name'];
+
+    $cat = new Category;
+    $cat->name = "category x";
+    $cat->save();
+
+
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+
+/* ==== RUN ==== */
+
+$slim->run();
