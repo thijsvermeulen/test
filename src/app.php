b99ebf2dc8e74955fb5fff351d6043ea3ad0a7d4
Thijs Vermeulen <thijsver@xs4all.nl>
2018-02-18 22:57:53 +0100

    improved config

--- src/app.php
+++ src/app.php
@@ -2,15 +2,15 @@
 
 function run() {
 
-
+    $conf = array_merge( parse_ini_file('../conf-default.ini'), parse_ini_file('../conf-local.ini'));
 
     $eloquent = new Illuminate\Database\Capsule\Manager;
     $eloquent->addConnection([
         'driver'    => 'mysql',
-        'host'      => 'localhost',
-        'database'  => 'database',
-        'username'  => 'root',
-        'password'  => 'a3d5g7',
+        'host'      => $conf['db_host'],
+        'database'  => $conf['db_name'],
+        'username'  => $conf['db_user'],
+        'password'  => $conf['db_pass'],
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
